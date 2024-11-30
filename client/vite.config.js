555352f34603bb0bee323d4c0e2a922cdbb3b96e
amit712singhal <rakshit.singhal712@gmail.com>
2024-11-30 13:30:02 +0530

    Format Vite configuration for consistency

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig( {
+  plugins: [ react() ],
+} )
