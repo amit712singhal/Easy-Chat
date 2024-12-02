39bb9d0f38a2e81a0db53e6041fcad099c24c51c
amit712singhal <rakshit.singhal712@gmail.com>
2024-12-02 19:32:51 +0530

    create server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect( process.env.MONGO_URL ).then( () => { console.log( 'MongoDB connected!' ) } ).catch( ( err ) => { console.log( err ) } );
+
+const app = express();
+
+app.listen( 3000, () =>
+{
+  console.log( 'Server is running on port 3000!' );
+} );
