4a4566ff6990c5eac5cfd256427a2e2205f2a009
amit712singhal <rakshit.singhal712@gmail.com>
2024-12-02 18:50:20 +0530

    Add Header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,16 @@
+const flowbite = require( "flowbite-react/tailwind" );
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
